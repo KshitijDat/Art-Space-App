b6435dfa64fc999a17a53b808ac3ab9a9cc87466
Kshitij Datta <71891062+KshitijDat@users.noreply.github.com>
2023-12-31 23:50:03 +0530

    Create ArtSpace.kt

---
+++ ArtSpace.kt
@@ -0,0 +1,121 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessImage("Flower", "Digital Art")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CheckImage(str1: String, str2: String, str3: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.flower_removebg_preview)
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )  {
+        Image (
+            painter = image,
+            contentDescription = "flower",
+            modifier = Modifier
+                .size(160.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = str1,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 38.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = str2,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text (
+            text = str3
+        )
+    }
+}
+
+
+@Composable
+fun BusinessImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        CheckImage(
+            str1 = message,
+            str2 = from,
+            str3 = "The work on this is still under progress... Sorry for the delay",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+                .align(alignment = Alignment.Center)
+        )
+    }
+}
